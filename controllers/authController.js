8ef02257e3c2f512ce56a11caa419626bcb95fda
Fady Alamir <110240643+fadyalamir@users.noreply.github.com>
2025-12-29 23:07:28 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const { User, validationRegisterUser, validationLoginUser} = require("../models/User");
-const { VerificationToken } = require('../models/VerificationToken.js');
+const { VerificationToken } = require('../models/verificationToken.js');
 const EmailTemplate = require("../utils/EmailTemplate.js");
 const crypto = require("crypto");
 const sendEmail = require("../utils/sendEmail.js");
